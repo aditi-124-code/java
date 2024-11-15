1a9311f72f68c7916a6ed1404c939ee1c9e4cabf
aditi-124-code <adititiwari2626@gmail.com>
2024-11-15 15:13:22 +0530

    Add files via upload

---
+++ question6.java
@@ -0,0 +1,24 @@
+package Assignment_2;
+
+class MathOperations {
+    
+    int add(int a, int b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MathOperations math = new MathOperations();
+        System.out.println("Sum of two integers: " + math.add(5, 10));
+        System.out.println("Sum of three integers: " + math.add(5, 10, 15)); 
+        System.out.println("Sum of two doubles: " + math.add(5.5, 10.5)); 
+    }
+}
+
